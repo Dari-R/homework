e62d6d3ab7b40a5126c36c2edbd95a3c1009fc37
Darina840 <darya2002rybnikova2013@yandex.ru>
2025-06-11 11:12:47 +0300

    review 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,28 +6,32 @@ import (
 	"strings"
 )
 
-const usdTorub = 79.74
-const usdToeur = 0.88
-const usd, eur, rub = "USD", "EUR", "RUB"
-
-func inputFunc() (string, float64, string) {
+func main() {
 	var money float64
-	var firstVal, secondVal string
-	fmt.Println("Введите валюту которую хотите конвертировать(EUR, USD, RUB)")
+	var firstVal, secondVal, repeatVar string
+	currencies := map[string]float64{
+		"USD": 1,
+		"RUB": 79.74,
+		"EUR": 0.88,
+	}
+	fmt.Println("__Калькулятор валют: EUR, USD, RUB__")
 	for {
-		_, err := fmt.Scan(&firstVal)
-		firstVal = strings.ToUpper(firstVal)
-		if err != nil {
-			fmt.Println("Ошибка ввода")
-		} else if firstVal != usd && firstVal != eur && firstVal != rub {
-			fmt.Println("Ошибка, еще раз введите валюту которую хотите конвертировать(EUR, USD, RUB)")
-		} else {
+		firstVal, money, secondVal = inputFunc(currencies)
+		fmt.Println(converter(firstVal, secondVal, money,currencies))
+		fmt.Println("Хотите повторить? (y/n)")
+		fmt.Scan(&repeatVar)
+		if strings.ToLower(repeatVar) != "y" && strings.ToLower(repeatVar) != "Y" {
 			break
 		}
 	}
-	fmt.Println("Отлично! Введите сумму:")
-	var checkMoney string
+}
+
+func inputFunc(currencies map[string]float64) (string, float64, string) {
+	var money float64
+	var firstVal, secondVal, checkMoney string 
 	var err error
+	valConv(currencies, &firstVal)
+	fmt.Println("Отлично! Введите сумму:")
 	for {
 		fmt.Scan(&checkMoney)
 		money, err = strconv.ParseFloat(checkMoney, 64)
@@ -37,57 +41,28 @@ func inputFunc() (string, float64, string) {
 			break
 		}
 	}
-	fmt.Println("Введите валюту  в которую хотите перевести(EUR, USD, RUB)")
+	valConv(currencies, &secondVal)
+	return firstVal, money, secondVal
+}
+
+func valConv(currencies map[string]float64, val *string){
+	fmt.Println("Введите валюту(EUR, USD, RUB)")
 	for {
-		_, err := fmt.Scan(&secondVal)
-		secondVal = strings.ToUpper(secondVal)
+		_, err := fmt.Scan(val)
+		*val = strings.ToUpper(*val)
 		if err != nil {
 			fmt.Println("Ошибка ввода")
-		} else if secondVal != usd && secondVal != eur && secondVal != rub {
-			fmt.Println("Ошибка, еще раз введите валюту которую хотите конвертировать(EUR, USD, RUB)")
+		} else if _, ok := currencies[*val]; !ok{
+			fmt.Println("Неверная валюта. Доступны только: EUR, USD, RUB. Повторите ввод.")
 		} else {
 			break
 		}
 	}
-	return firstVal, money, secondVal
 }
-
-func converter(firstVal, secondVal string, money float64) string {
+func converter(firstVal, secondVal string, money float64, currencies map[string]float64) string {
 
 	var result string
-	switch {
-	case firstVal == rub && secondVal == usd:
-		result = fmt.Sprintf("%2.f рублей это %.2f долларах \n", money, money/usdTorub)
-	case firstVal == rub && secondVal == eur:
-		result = fmt.Sprintf("%2.f рублей это %.2f евро\n", money, money/usdTorub*usdToeur)
-	case firstVal == usd && secondVal == rub:
-		result = fmt.Sprintf("%2.f долларов это %.2f рублей\n", money, money*usdTorub)
-	case firstVal == usd && secondVal == eur:
-		result = fmt.Sprintf("%2.f долларов это %.2f евро\n", money, money*usdToeur)
-	case firstVal == eur && secondVal == rub:
-		result = fmt.Sprintf("%2.f евро это %.2f рублей\n", money, money/usdToeur*usdTorub)
-	case firstVal == eur && secondVal == usd:
-		result = fmt.Sprintf("%2.f евро это %.2f долларов\n", money, money/usdToeur)
-	case firstVal == eur && secondVal == eur:
-		result = fmt.Sprintf("%2.f евро это %.2f евро\n", money, money)
-	case firstVal == usd && secondVal == usd:
-		result = fmt.Sprintf("%2.f долларов это %.2f долларов\n", money, money)
-	case firstVal == rub && secondVal == rub:
-		result = fmt.Sprintf("%2.f рублей это %.2f рублей\n", money, money)
-	}
+	sum := money*currencies[secondVal]/currencies[firstVal]
+	result = fmt.Sprintf("%.2f %s это %.2f в %s \n", money, firstVal, sum, secondVal )
 	return result
-}
-func main() {
-	fmt.Println("__Калькулятор для перевода из евро в рубли__")
-	var money float64
-	var firstVal, secondVal, repeatVar string
-	for {
-		firstVal, money, secondVal = inputFunc()
-		fmt.Println(converter(firstVal, secondVal, money))
-		fmt.Println("Хотите повторить? (y/n)")
-		fmt.Scan(&repeatVar);
-		if repeatVar != "y" && repeatVar != "Y"{
-			break
-		}
-	}
 }
---
+++ 2-calc/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var operation int
+	var str string
+
+	for {
+		fmt.Println("Что вы хотите вычислить? (1.AVG - среднее 2. SUM - сумму 3. MED - медиану 4.Выход)\n Введите номер:")
+		fmt.Scan(&operation)
+		if operation != 1 && operation != 2 && operation != 3 && operation != 4 {
+			fmt.Println("Неверная операция.")
+			continue
+		} else if operation == 4 {
+			break
+		}
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Введите числа через запятую: ")
+		str, _ = reader.ReadString('\n')
+		resPrint(&operation, str)
+		if operation == 4 {
+			break
+		}
+	}
+}
+
+func resPrint(operation *int, str string) {
+	var numArr []float64
+	var err error
+	if numArr, err = convStr(str); err != nil {
+		fmt.Println(err)
+		return
+	}
+	switch *operation {
+	case 1:
+		fmt.Println("Среднее:", avg(numArr))
+	case 2:
+		fmt.Println("Сумма:", sum(numArr))
+	case 3:
+		fmt.Println("Медиана:", med(numArr))
+	}
+	fmt.Println("Продолжить? 0 - да, 4 - выход")
+	fmt.Scan(operation)
+}
+func convStr(str string) ([]float64, error) {
+	str = strings.TrimSpace(str)
+	str = strings.ReplaceAll(str, " ", "")
+	parts := strings.Split(str, ",")
+
+	numArr := make([]float64, len(parts))
+	for i, val := range parts {
+		num, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return nil, fmt.Errorf("ошибка преобразования: %v", err)
+		}
+		numArr[i] = num
+	}
+	return numArr, nil
+}
+func sum(numArr []float64) float64 {
+	var sum float64
+	for i := 0; i < len(numArr); i++ {
+		sum += numArr[i]
+	}
+	return sum
+}
+
+func avg(numArr []float64) float64 {
+	avg := sum(numArr) / float64(len(numArr))
+	return avg
+}
+
+func med(numArr []float64) float64 {
+	var num float64
+	for i := 0; i < len(numArr); i++ {
+		for j := 0; j < len(numArr)-i-1; j++ {
+			if numArr[j+1] < numArr[j] {
+				num = numArr[j]
+				numArr[j] = numArr[j+1]
+				numArr[j+1] = num
+			}
+		}
+	}
+	return numArr[len(numArr)/2]
+}
