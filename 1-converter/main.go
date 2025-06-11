c83b4522f66e519a99512f3c37ef8ddef024877d
Darina840 <darya2002rybnikova2013@yandex.ru>
2025-06-11 15:49:26 +0300

    review 2.7

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,66 +2,49 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
+var currencies = map[string]float64{
+	"USD": 1.0,
+	"RUB": 79.74,
+	"EUR": 0.88,
+}
+
+var operations = map[string]func(float64, string, string) string{
+	"CONVERT": func(amount float64, from, to string) string {
+		fromRate, fromOk := currencies[strings.ToUpper(from)]
+		toRate, toOk := currencies[strings.ToUpper(to)]
+		if !fromOk || !toOk {
+			return "Ошибка: Неверная валюта. Доступны только EUR, USD, RUB."
+		}
+		converted := amount * toRate / fromRate
+		return fmt.Sprintf("%.2f %s это %.2f в %s", amount, strings.ToUpper(from), converted, strings.ToUpper(to))
+	},
+}
+
 func main() {
-	var money float64
-	var firstVal, secondVal, repeatVar string
-	currencies := map[string]float64{
-		"USD": 1,
-		"RUB": 79.74,
-		"EUR": 0.88,
+	if len(os.Args) != 4 {
+		fmt.Println("Использование: go run main.go <из_валюты> <сумма> <в_валюту>")
+		os.Exit(1)
 	}
-	fmt.Println("__Калькулятор валют: EUR, USD, RUB__")
-	for {
-		firstVal, money, secondVal = inputFunc(currencies)
-		fmt.Println(converter(firstVal, secondVal, money,&currencies))
-		fmt.Println("Хотите повторить? (y/n)")
-		fmt.Scan(&repeatVar)
-		if strings.ToLower(repeatVar) != "y" && strings.ToLower(repeatVar) != "Y" {
-			break
-		}
-	}
-}
 
-func inputFunc(currencies map[string]float64) (string, float64, string) {
-	var money float64
-	var firstVal, secondVal, checkMoney string 
-	var err error
-	valConv(&currencies, &firstVal)
-	fmt.Println("Отлично! Введите сумму:")
-	for {
-		fmt.Scan(&checkMoney)
-		money, err = strconv.ParseFloat(checkMoney, 64)
-		if err != nil {
-			fmt.Println("Неккоректный ввод, введите сумму еще раз")
-		} else {
-			break
-		}
-	}
-	valConv(&currencies, &secondVal)
-	return firstVal, money, secondVal
-}
+	from := os.Args[1]
+	amountStr := os.Args[2]
+	to := os.Args[3]
 
-func valConv(currencies * map[string]float64, val *string){
-	fmt.Println("Введите валюту(EUR, USD, RUB)")
-	for {
-		_, err := fmt.Scan(val)
-		*val = strings.ToUpper(*val)
-		if err != nil {
-			fmt.Println("Ошибка ввода")
-		} else if _, ok := (*currencies)[*val]; !ok{
-			fmt.Println("Неверная валюта. Доступны только: EUR, USD, RUB. Повторите ввод.")
-		} else {
-			break
-		}
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		fmt.Println("Ошибка: сумма должна быть числом")
+		os.Exit(1)
 	}
-}
-func converter(firstVal, secondVal string, money float64, currencies *map[string]float64) string {
-	var result string
-	sum := money*(*currencies)[secondVal]/(*currencies)[firstVal]
-	result = fmt.Sprintf("%.2f %s это %.2f в %s \n", money, firstVal, sum, secondVal )
-	return result
+
+	op := "CONVERT"
+	if operation, ok := operations[op]; ok {
+		fmt.Println(operation(amount, from, to))
+	} else {
+		fmt.Println("Ошибка: операция не поддерживается")
+	}
 }
