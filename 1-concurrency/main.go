c6121a2577b4424239cfa1dd418059dd08cd0123
Darina840 <darya2002rybnikova2013@yandex.ru>
2025-08-20 19:35:43 +0300

    1-concurrency try 2

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -3,47 +3,39 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 )
 
 func main() {
-	ch1, ch2 := make(chan int, 10), make(chan int, 10)
-	var wg1, wg2 sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg1.Add(1)
-		go func() {
-			makeSlice(ch1)
-			wg1.Done()
-		}()
-	}
+	ch1, ch2 := make(chan []int), make(chan int, 10)
 	go func() {
-		wg1.Wait()
+		makeSlice(ch1)
 		close(ch1)
 	}()
-	for v := range ch1 {
-		wg2.Add(1)
-		go func(x int) {
-			sqrtFunc(x, ch2)
-			wg2.Done()
-		}(v)
 
-	}
 	go func() {
-		wg2.Wait()
-		close(ch2)
+		sl:=<-ch1
+		for _, s := range sl {
+			sqrtFunc(s, ch2)
+		}
 
+		close(ch2)
 	}()
+
 	for v := range ch2 {
 		fmt.Println(v)
 	}
 
 }
 
-func makeSlice(ch chan int) {
-	a := rand.Intn(100) + 1
-	ch <- a
+func makeSlice(ch chan []int) {
+	sl := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		a := rand.Intn(100) + 1
+		sl[i] = a
+	}
+	ch <- sl
 }
 
 func sqrtFunc(res int, ch2 chan int) {
-	ch2 <- (res * res)
+	ch2 <- res * res
 }
